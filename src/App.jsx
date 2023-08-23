2fc3ade22f4359ff84b3a67010115025537cdef2
Enam <mdenamahmedchowdhury@gmail.com>
2023-08-23 22:14:36 +0600

    add topBar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
+    
       <div className="w-full h-screen text-4xl text-blue-700 flex items-center justify-center">
         Bismillah
       </div>
---
+++ src/component/topBar/TopBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./topBar.css";
+
+const TopBar = () => {
+  return <div>TopBar</div>;
+};
+
+export default TopBar;
