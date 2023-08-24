516215358e561296c4386bd3845796f0507b390e
Enam <mdenamahmedchowdhury@gmail.com>
2023-08-24 23:16:34 +0600

    start work on Sign Up left banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
+import SignUpSignInLeftBanner from "./component/signUpSignInLeftBanner/SignUpSignInLeftBanner";
 
 function App() {
   return (
     <>
     
-      <div className="w-full h-screen text-4xl text-blue-700 flex items-center justify-center">
-        Bismillah
-      </div>
+      <SignUpSignInLeftBanner/>
     </>
   );
 }
--- src/component/signUpSignInLeftBanner/SignUpSignInLeftBanner.jsx
+++ src/component/signUpSignInLeftBanner/SignUpSignInLeftBanner.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import "./signUpSignInLeftBanner.css"
+import LogoWhite from "../../assets/logoWhite.png"
 
 const SignUpSignInLeftBanner = () => {
   return (
-    <div>SignUpSignInLeftBanner</div>
+    <div className='signUp-signIn-left-banner  box-border h-screen w-3/6 p-10'>
+      <div className='logo'>
+        <img src={LogoWhite} alt="" />
+      </div>
+
+      <div className='slider'>
+
+      </div>
+    </div>
   )
 }
 
